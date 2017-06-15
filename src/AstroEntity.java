9a7557480dd70b57d0f8326fdcddf020949e465d
garlapaty <suprajagarlapaty@gmail.com>
2017-06-15 15:07:27 -0400

    Planetarium Second Phase
    Commited the second phase

---
+++ src/AstroEntity.java
@@ -0,0 +1,9 @@
+
+public abstract class AstroEntity {
+
+	public static void main(String[] args) {
+		System.out.println("Planet");
+
+	}
+
+}
---
+++ src/Planet.java
@@ -0,0 +1,13 @@
+
+public abstract class Planet {
+
+	public Planet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
